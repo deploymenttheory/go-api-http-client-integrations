4154a64d960366a06657f843621796a3fce3be24
Joseph Little <joelittle11@live.com>
2024-06-10 14:17:35 +0100

    debugging

--- jamfprointegration/auth.go
+++ jamfprointegration/auth.go
@@ -3,8 +3,6 @@ package jamfprointegration
 import (
 	"errors"
 	"time"
-
-	"go.uber.org/zap"
 )
 
 const (
@@ -26,8 +24,6 @@ type authInterface interface {
 func (j *Integration) checkRefreshToken() error {
 	var err error
 
-	j.Logger.Debug("Checking and refreshing token")
-
 	if j.auth.tokenEmpty() {
 		j.Logger.Warn(tokenEmptyWarnString)
 	}
@@ -38,9 +34,6 @@ func (j *Integration) checkRefreshToken() error {
 		if err != nil {
 			return err
 		}
-
-		j.Logger.Debug("Bools:", zap.Bool("expired", j.auth.tokenExpired()), zap.Bool("in buffer", j.auth.tokenInBuffer()), zap.Bool("empty", j.auth.tokenEmpty()))
-		j.Logger.Debug("Vars", zap.String("exp time", j.auth.getExpiryTime().String()))
 
 		// Protects against bad token lifetime/buffer combinations (infinite loops)
 		if j.auth.tokenExpired() || j.auth.tokenInBuffer() {
