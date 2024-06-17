75edaa930e9f3e8709b87202d0af4d0458fc6286
Joseph Little <joelittle11@live.com>
2024-06-17 10:59:06 +0100

    Load balancer workaround implemented

--- jamfprointegration/constants.go
+++ jamfprointegration/constants.go
@@ -2,8 +2,13 @@ package jamfprointegration
 
 // Endpoint constants represent the URL suffixes used for Jamf API token interactions.
 const (
+	// Auth
 	oAuthTokenEndpoint      string = "/api/oauth/token"
 	bearerTokenEndpoint     string = "/api/v1/auth/token"
 	invalidateTokenEndpoint string = "/api/v1/auth/invalidate-token"
 	keepAliveTokenEndpoint  string = "/api/v1/auth/keep-alive"
+
+	// Load balancer workaround
+	LoadBalancerTargetCookie string = "jpro-ingress"
+	LoadBalancerPollCount    int    = 5
 )
--- jamfprointegration/load_balancer_workaround.go
+++ jamfprointegration/load_balancer_workaround.go
@@ -1,12 +1,19 @@
 package jamfprointegration
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"slices"
+)
 
-func getAllLoadBalancers(url string, pollCount int) *[]string {
-	for i := 0; i < pollCount+1; i++ {
-		fmt.Printf("iter-{%v}", i)
+func (j *Integration) GetLoadBalancer(urlString string) (string, error) {
+	allBalancers, err := j.getAllLoadBalancers(urlString)
+	if err != nil {
+		return "", fmt.Errorf("error getting load balancers: %v", err)
 	}
-	return nil
+
+	chosenCookie := chooseMostAlphabeticalString(*allBalancers)
+	return chosenCookie, nil
 }
 
 func chooseMostAlphabeticalString(strings []string) string {
@@ -22,4 +29,45 @@ func chooseMostAlphabeticalString(strings []string) string {
 	}
 
 	return mostAlphabeticalStr
+}
+
+func (j *Integration) getAllLoadBalancers(urlString string) (*[]string, error) {
+	var cookiesList []*http.Cookie
+	var outList []string
+	var err error
+	var req *http.Request
+	var resp *http.Response
+	client := http.Client{}
+
+	for i := 0; i < LoadBalancerPollCount; i++ {
+		req, err = http.NewRequest("GET", urlString, nil)
+		if err != nil {
+			return nil, fmt.Errorf("error creating request: %v", err)
+		}
+
+		err = j.PrepRequestParamsAndAuth(req)
+		if err != nil {
+			return nil, fmt.Errorf("error populating auth: %v", err)
+		}
+
+		resp, err = client.Do(req)
+		if err != nil {
+			return nil, fmt.Errorf("error sending req: %v", err)
+		}
+
+		respCookies := resp.Cookies()
+
+		for i, v := range respCookies {
+			if v.Name == LoadBalancerTargetCookie {
+				cookiesList = append(cookiesList, respCookies[i])
+				outList = append(outList, v.Value)
+			}
+		}
+
+	}
+
+	slices.Sort(outList)
+	newList := slices.Compact(outList)
+	return &newList, nil
+
 }
