686c536c7a45e41084977cb30c19d0cc65729795
Joseph Little <joelittle11@live.com>
2024-06-17 09:57:53 +0100

    loadbalancerworkaround

--- jamfprointegration/load_balancer_workaround.go
+++ jamfprointegration/load_balancer_workaround.go
@@ -1,5 +1,25 @@
 package jamfprointegration
 
-func getAllLoadBalancers(url string) *[]string {
+import "fmt"
+
+func getAllLoadBalancers(url string, pollCount int) *[]string {
+	for i := 0; i < pollCount+1; i++ {
+		fmt.Printf("iter-{%v}", i)
+	}
 	return nil
+}
+
+func chooseMostAlphabeticalString(strings []string) string {
+	if len(strings) == 0 {
+		return ""
+	}
+
+	mostAlphabeticalStr := strings[0]
+	for _, str := range strings[1:] {
+		if str < mostAlphabeticalStr {
+			mostAlphabeticalStr = str
+		}
+	}
+
+	return mostAlphabeticalStr
 }
