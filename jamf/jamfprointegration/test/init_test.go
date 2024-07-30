40e3dfb976235777a917d359d199626eb0e75cf1
Joseph Little <joelittle11@live.com>
2024-07-30 14:17:46 +0100

    integration testing complete for builders

--- jamf/jamfprointegration/test/init_test.go
+++ jamf/jamfprointegration/test/init_test.go
@@ -11,6 +11,9 @@ import (
 	"go.uber.org/zap"
 )
 
+// API integration has a settable expiry period and is set to 60s
+// Account tokens do not have this. I think the expiry is an hour.
+
 const (
 	ENV_KEY_JAMFPRO_FQDN  = "TEST_JAMFPRO_FQDN"
 	ENV_KEY_CLIENT_ID     = "TEST_JAMFPRO_CLIENT_ID"
@@ -36,7 +39,7 @@ func Test_BuildWithOAuth(t *testing.T) {
 		wantErr bool
 	}{
 		{
-			name: "All vars set correctly",
+			name: "all vars set correctly",
 			args: args{
 				jamfProFQDN:       os.Getenv(ENV_KEY_JAMFPRO_FQDN),
 				clientId:          os.Getenv(ENV_KEY_CLIENT_ID),
@@ -61,6 +64,32 @@ func Test_BuildWithOAuth(t *testing.T) {
 			},
 			wantErr: true,
 		},
+		{
+			name: "no client id",
+			args: args{
+				jamfProFQDN:       os.Getenv(ENV_KEY_JAMFPRO_FQDN),
+				clientId:          "",
+				clientSecret:      os.Getenv(ENV_KEY_CLIENT_SECRET),
+				bufferPeriod:      10 * time.Minute,
+				hideSensitiveData: true,
+				executor:          &httpclient.ProdExecutor{Client: &http.Client{}},
+				Sugar:             newSugaredDevelopmentLogger(),
+			},
+			wantErr: true,
+		},
+		{
+			name: "no client secret",
+			args: args{
+				jamfProFQDN:       os.Getenv(ENV_KEY_JAMFPRO_FQDN),
+				clientId:          os.Getenv(ENV_KEY_CLIENT_ID),
+				clientSecret:      "",
+				bufferPeriod:      10 * time.Minute,
+				hideSensitiveData: true,
+				executor:          &httpclient.ProdExecutor{Client: &http.Client{}},
+				Sugar:             newSugaredDevelopmentLogger(),
+			},
+			wantErr: true,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -69,9 +98,88 @@ func Test_BuildWithOAuth(t *testing.T) {
 				t.Errorf("BuildWithOAuth() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
-			// if !reflect.DeepEqual(got, tt.want) {
-			// 	t.Errorf("BuildWithOAuth() = %v, want %v", got, tt.want)
-			// }
+			// Only testing for error as cannot predict pointers inside Integration output. Error is enough to deem success.
+		})
+	}
+}
+
+func TestBuildWithBasicAuth(t *testing.T) {
+	type args struct {
+		jamfProFQDN       string
+		Sugar             *zap.SugaredLogger
+		bufferPeriod      time.Duration
+		username          string
+		password          string
+		hideSensitiveData bool
+		executor          httpclient.HTTPExecutor
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    *jamfprointegration.Integration
+		wantErr bool
+	}{
+		{
+			name: "all vars set correctly",
+			args: args{
+				jamfProFQDN:       os.Getenv(ENV_KEY_JAMFPRO_FQDN),
+				username:          os.Getenv(ENV_KEY_USERNAME),
+				password:          os.Getenv(ENV_KEY_PASSWORD),
+				bufferPeriod:      10 * time.Second,
+				hideSensitiveData: true,
+				executor:          &httpclient.ProdExecutor{Client: &http.Client{}},
+				Sugar:             newSugaredDevelopmentLogger(),
+			},
+			wantErr: false,
+		},
+		{
+			name: "buffer period too long",
+			args: args{
+				jamfProFQDN:       os.Getenv(ENV_KEY_JAMFPRO_FQDN),
+				username:          os.Getenv(ENV_KEY_USERNAME),
+				password:          os.Getenv(ENV_KEY_PASSWORD),
+				bufferPeriod:      100 * time.Minute,
+				hideSensitiveData: true,
+				executor:          &httpclient.ProdExecutor{Client: &http.Client{}},
+				Sugar:             newSugaredDevelopmentLogger(),
+			},
+			wantErr: true,
+		},
+		{
+			name: "no username",
+			args: args{
+				jamfProFQDN:       os.Getenv(ENV_KEY_JAMFPRO_FQDN),
+				username:          "",
+				password:          os.Getenv(ENV_KEY_PASSWORD),
+				bufferPeriod:      100 * time.Minute,
+				hideSensitiveData: true,
+				executor:          &httpclient.ProdExecutor{Client: &http.Client{}},
+				Sugar:             newSugaredDevelopmentLogger(),
+			},
+			wantErr: true,
+		},
+		{
+			name: "no password",
+			args: args{
+				jamfProFQDN:       os.Getenv(ENV_KEY_JAMFPRO_FQDN),
+				username:          os.Getenv(ENV_KEY_USERNAME),
+				password:          "",
+				bufferPeriod:      100 * time.Minute,
+				hideSensitiveData: true,
+				executor:          &httpclient.ProdExecutor{Client: &http.Client{}},
+				Sugar:             newSugaredDevelopmentLogger(),
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			_, err := jamfprointegration.BuildWithBasicAuth(tt.args.jamfProFQDN, tt.args.Sugar, tt.args.bufferPeriod, tt.args.username, tt.args.password, tt.args.hideSensitiveData, tt.args.executor)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("BuildWithBasicAuth() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			// Only testing for error as cannot predict pointers inside Integration output. Error is enough to deem success.
 		})
 	}
 }
