bf11d7f1dbae438eb8a0a6e82f30017a84999e7a
Bobby <bobby.james.williams@outlook.com>
2025-03-03 11:13:16 +0000

    re-adding loadbalancer jammy

--- jamf/jamfprointegration/load_balancer_workaround.go
+++ jamf/jamfprointegration/load_balancer_workaround.go
@@ -3,7 +3,8 @@ package jamfprointegration
 import (
 	"fmt"
 	"net/http"
-	"slices"
+	"time"
+	"strings"
 )
 
 // GetSessionCookies retrieves all cookies from the current session
@@ -25,6 +26,8 @@ func (j *Integration) GetLoadBalancer(urlString string) (string, error) {
 	}
 
 	chosenCookie := chooseMostAlphabeticalString(*allBalancers)
+	// chosenCookie = "9a794a6f22fea3b1"
+	j.Sugar.Debugf("Chosen Cookie:%v ", chosenCookie)
 	return chosenCookie, nil
 }
 
@@ -46,39 +49,73 @@ func chooseMostAlphabeticalString(strings []string) string {
 
 // TODO migrate strings
 func (j *Integration) getAllLoadBalancers(urlString string) (*[]string, error) {
+	// j.Sugar.Debug("LOGHERE1")
+	j.Sugar.Debug("Starting cookie magic (has this propogated?)")
 	var outList []string
 	var err error
 	var req *http.Request
 	var resp *http.Response
+	var iterations int
 
-	for i := 0; i < LoadBalancerPollCount; i++ {
+	iterations = 0
+	startTimeEpoch := time.Now().Unix()
+	// j.Sugar.Debugf("Start time: %d", startTimeEpoch)
+	endTimeEpoch := startTimeEpoch + int64(LoadBalancerTimeOut.Seconds())
+	// j.Sugar.Debugf("End Time: %d", endTimeEpoch)
+
+	for i := time.Now().Unix(); i < endTimeEpoch; {
+		// j.Sugar.Debugf("################Iterations:%v####################", iterations)
 		req, err = http.NewRequest("GET", urlString, nil)
 		if err != nil {
 			return nil, fmt.Errorf("error creating request: %v", err)
 		}
 
+		// Auth required on login screen or 404
 		err = j.PrepRequestParamsAndAuth(req)
 		if err != nil {
 			return nil, fmt.Errorf("error populating auth: %v", err)
 		}
 
-		resp, err = j.http.Do(req)
+		resp, err = j.httpExecutor.Do(req)
 		if err != nil {
 			return nil, fmt.Errorf("error sending req: %v", err)
 		}
 
 		respCookies := resp.Cookies()
+		// j.Sugar.Debugf("Cookies got: %+v", respCookies)
 
 		for _, v := range respCookies {
 			if v.Name == LoadBalancerTargetCookie {
-				outList = append(outList, v.Value)
+				strippedCookie := strings.TrimSpace(v.Value)
+				// j.Sugar.Debugf("Removing Whitespace. Before #%v# After #%v#", v.Value, strippedCookie)
+				// j.Sugar.Debugf("Appending: %v", strippedCookie)
+				outList = append(outList, strippedCookie)
 			}
 		}
+		// j.Sugar.Debugf("BEGIN DUPE REMOVAL. OUTLIST: %v", outList)
+		
+		uniqueMap := make(map[string]bool)
+	
+		for _, str := range outList {
+			uniqueMap[str] = true
+		}
+		
+		cookieDupesRemoved := make([]string, 0, len(uniqueMap))
+		
+		for str := range uniqueMap {
+			cookieDupesRemoved = append(cookieDupesRemoved, str)
+		}
 
+
+		// j.Sugar.Debugf("DUPES REMOVED: %v", cookieDupesRemoved)
+		if len(cookieDupesRemoved) > 1 { 
+			j.Sugar.Debugf("### COMPLETED COOKIE MAGIC ### Dupes removed: %v, outlist: %v", cookieDupesRemoved, outList)
+			break
+		}
+
+		i = time.Now().Unix()
+		iterations += 1
 	}
-
-	slices.Sort(outList)
-	newList := slices.Compact(outList)
-	return &newList, nil
+	return &outList, nil
 
 }
