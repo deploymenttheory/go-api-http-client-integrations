0c79e1b7bbc75dfc0224766a6e4ae2b091a77422
Neil Martin <neil.martin@jamf.com>
2025-11-12 15:22:11 +0000

    fix: Correct OAuth token endpoint URL in constants

--- jamf/jamfprointegration/constants.go
+++ jamf/jamfprointegration/constants.go
@@ -5,14 +5,13 @@ import "time"
 // Endpoint constants represent the URL suffixes used for Jamf API token interactions.
 const (
 	// Auth
-	oAuthTokenEndpoint      string = "/api/oauth/token"
+	oAuthTokenEndpoint      string = "/api/v1/oauth/token"
 	bearerTokenEndpoint     string = "/api/v1/auth/token"
 	invalidateTokenEndpoint string = "/api/v1/auth/invalidate-token"
 	keepAliveTokenEndpoint  string = "/api/v1/auth/keep-alive"
 
 	// Load balancer workaround
-	LoadBalancerTargetCookie string = "jpro-ingress"
-	LoadBalancerPollCount    int    = 5
+	LoadBalancerTargetCookie string        = "jpro-ingress"
+	LoadBalancerPollCount    int           = 5
 	LoadBalancerTimeOut      time.Duration = 7 * time.Second
-
 )
